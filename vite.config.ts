4647cb66247a3e65cc8dc4f4e1e63c7cde9b71c4
aleksandr <al.zhuk@andersenlab.com>
2024-10-01 19:36:10 +0300

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://rest.cryptoapis.io',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false,
-      },
-    },
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: '/Crypto-trading-app/',
+    
+  };
 });
